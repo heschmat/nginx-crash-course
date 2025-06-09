0bc50bc8f1d55522855d16abe78148f1edb153ac
Hesch <heschmat.seyedi@gmail.com>
2025-06-09 22:11:38 +0200

    chore: add docker-compose.yaml

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
+const appName = process.env.APP_NAME || 'Unknown App';
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
+  console.log(`[${appName}] Received GET /`);
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`[${appName}] Server running at http://localhost:${PORT}`);
 });
